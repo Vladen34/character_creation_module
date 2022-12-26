0423bd62337860ed68dd3c78db76267cb2e0c989
Vladislav Sotskov <ti@ayavlad.ru>
2022-12-26 20:43:37 +0300

    Update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
+    """Нужно описание каждой функции."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
